1842a5ec3019d32a2b19f6ec40ee790a356b2213
19b1num0915 <75355855+19b1num0915@users.noreply.github.com>
2022-06-22 18:07:30 +0800

    Update ExampleResource.java
    sdahashd

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,13 +1,17 @@
 package org.acme;
 
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.pgclient.PgPool;
 
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
 import org.acme.Model.Users;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestResponse;
+
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
@@ -16,7 +20,6 @@ import javax.ws.rs.core.*;
 @Path("/hello")
 public class ExampleResource {
     private static final Logger logger = Logger.getLogger(ExampleResource.class);
-    public static final String TOKEN = "dev";
 
     @Inject
     PgPool client;
@@ -24,26 +27,28 @@ public class ExampleResource {
     @Inject
     Users user;
 
-
-
-    @Path("/sda")
+    @Path("/get")
     @GET
     public Multi<Users> get() {
         return Users.findAll(client);
     }
 
-
     @GET
     @Path("{id}")
     public Uni<Response> getID(Long id) {
-
         logger.infov("id={0}", id);
-
         return Users.findById(client, id)
                 .onItem().transform(user -> user != null ? Response.ok(user) : Response.status(RestResponse.Status.NOT_FOUND))
                 .onItem().transform(Response.ResponseBuilder::build);
     }
 
+    @DELETE
+    @Path("/delete/{id}")
+    public Uni<Response> deleteID(Long id) {
+        return Users.delete(client, id)
+                .onItem().transform(deleted -> deleted ? RestResponse.Status.NO_CONTENT : RestResponse.Status.NOT_FOUND)
+                .onItem().transform(status -> Response.status(status).build());
+    }
 
     @Path("/add")
     @POST
@@ -60,10 +65,34 @@ public class ExampleResource {
         user.setPassword(body.getString("pass"));
         user.setType(body.getInteger("type"));
 
-        client.query("insert into Users(token1,name1,password1,phone,email,typeNumber) " +
-                        "values ('" + user.getToken() + "','" + user.getName() + "', '" + user.getPassword().hashCode() + "', " + user.getPhone() + ", '" + user.getEmail() + "', " + user.getType() + ")")
-                .execute().await().indefinitely();
+        /**
+         * password hash
+         */
+        user.setPassword(BcryptUtil.bcryptHash(user.getPassword()));
 
+
+        client.query("insert into Users(token1,name1,password1,phone,email,typeNumber) " +
+                        "values ('" + user.getToken() + "','" + user.getName() + "', '" + user.getPassword() + "', " + user.getPhone() + ", '" + user.getEmail() + "', " + user.getType() + ")")
+                .execute().await().indefinitely();
         return RestResponse.ResponseBuilder.ok(j).build();
+    }
+
+    @Path("login")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<RestResponse<JsonObject>> login(JsonObject body) {
+        JsonObject jret = new JsonObject();
+
+        String email = body.getString("email");
+        String pass = body.getString("password");
+
+
+        return client.query("SELECT count(*) as cnt FROM Users WHERE email='" + email + "' and password1='" + pass + "'")
+                .execute()
+                .onItem().transformToUni(rowset -> {
+                    return Uni.createFrom().item(rowset.iterator().next().getInteger(0));
+                })
+                .onItem().transform(count -> RestResponse.ResponseBuilder.ok(new JsonObject().put("count", count)).build());
     }
 }
